17e14194619a1be8ad53392539a2227be4862a8d
Alex Acer <drunkod@exampl.com>
2023-12-27 14:23:01 +0500

    change mongo to 4.4

--- scripts/docker_db_local.sh
+++ scripts/docker_db_local.sh
@@ -5,5 +5,5 @@ CONTAINER_NAME="lebkuchen-fm-db-local"
 if [ "$1" == "stop" ]; then
   docker stop $CONTAINER_NAME
 else
-  docker start $CONTAINER_NAME || docker run -p 27017:27017 --name=$CONTAINER_NAME -d mongo:latest
+  docker start $CONTAINER_NAME || docker run -p 27017:27017 --name=$CONTAINER_NAME -d mongo:4.4
 fi
